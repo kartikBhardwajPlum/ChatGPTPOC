ed396a580d7e710f149803560f07943f14662380
kartikBhardwajPlum <kartik.b@plumhq.com>
2023-03-06 10:41:39 +0530

    update UI

--- chat.py
+++ chat.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import requests
 
-st.set_page_config(page_title="Streamlit API Example", page_icon=":guardsman:", layout="wide")
+st.set_page_config(page_title="PlumChatBot", page_icon=":guardsman:", layout="wide")
 
 def api_call(query):
-    response = requests.post(url="https://employee-experience-service-uydiiifsnq-el.a.run.app/api/v1/chat/question?question="+query, headers={"x-plum-internal-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6IlZnUEg4SFNNYzBrVlZDYWZRRzRyIiwic291cmNlIjoiTUVNQkVSUyIsImVtYWlsSWQiOiJrZWVydGhpLnNpZGRlc2h3YXIrc3R1QHBsdW1ocS5jb20iLCJpYXQiOjE2NzU4NTk0NjEsImV4cCI6MTcyMjc4NTk0NjF9.hQtQeIlIpSyy1C2WkbaGZvcQEFWOq_JDzWb_Rwh0Mig"})
+    response = requests.post(url="https://employee-experience-service-uydiiifsnq-el.a.run.app/api/v1/chat/question?question="+query,headers={"x-plum-internal-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJZCI6IlZnUEg4SFNNYzBrVlZDYWZRRzRyIiwic291cmNlIjoiTUVNQkVSUyIsImVtYWlsSWQiOiJrZWVydGhpLnNpZGRlc2h3YXIrc3R1QHBsdW1ocS5jb20iLCJpYXQiOjE2NzU4NTk0NjEsImV4cCI6MTcyMjc4NTk0NjF9.hQtQeIlIpSyy1C2WkbaGZvcQEFWOq_JDzWb_Rwh0Mig"})
     return response.json()
 
 def main():
-    st.title("PIKU x GPT ")
-    query = st.text_input("Enter a question regarding your plum policy and benefits")
-    submit = st.button("Submit")
-    if submit:
-        result = api_call(query)
+    st.title("KUMAR OP ")
+    st.markdown("Ask a question about your policy.")
+    st.write("Here are some prompts:")
+    st.markdown("`* What is my maternity coverage?`")
+    st.markdown("`* How to file a claim?`")
+    st.markdown("`* Do I have any waiting periods?`")
+    st.markdown(">Tip: ")
+    query = st.text_input("Enter your question")
+    if st.button("Ask"):
+        with st.spinner("Generating response..."):
+            result = api_call(query)
         st.write("Tokens used:"+ str(result['tokens']))
         st.markdown(result['answer'].replace('\n', '<br>'), unsafe_allow_html=True)
 
